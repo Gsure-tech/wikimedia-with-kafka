9e3a0a242e33a5c6ba6b0e9c43c22867ad5f9462
gsure-tech <Geeescode@>
2023-10-26 06:47:06 +0100

    added the kafka producer wikimedia

---
+++ src/main/java/com/gsuretech/wikimediakafka/WikiMediaKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.gsuretech.wikimediakafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiMediaKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiMediaKafkaApplication.class, args);
+	}
+
+}
